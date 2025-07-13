79df0de0e4be6b214e3283300010c1c4aca13ce4
tepid <tepschmep@gmail.com>
2025-07-13 14:29:58 -0300

    Fixed non-existent `LobbyStatus` import

--- src/rd_matchmaking_bot/core/__init__.py
+++ src/rd_matchmaking_bot/core/__init__.py
@@ -1,3 +1,3 @@
 from rd_matchmaking_bot.core.dict_entry import DictEntry
-from rd_matchmaking_bot.core.lobby import Lobby, LobbyStatus
+from rd_matchmaking_bot.core.lobby import Lobby
 from rd_matchmaking_bot.core.user import User
